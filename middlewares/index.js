eaf14c9e97bb60017a87264cc320e3cad7ea2f49
Sonu <sonu@chapter247.com>
2022-01-31 11:36:59 +0530

    feat: added validation for signup

---
+++ middlewares/index.js
@@ -0,0 +1,3 @@
+module.exports = {
+  Validations: require("./Validate"),
+};
--- routes/UserRoute.js
+++ routes/UserRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const { UserController } = require("../controllers");
+const { Validations } = require("../middlewares");
 
 router.get("/", UserController.index);
-router.post("/", UserController.store);
+router.post("/", Validations.validateSignup, UserController.store);
 router.put("/:id", UserController.update);
 router.delete("/", UserController.destroy);
 router.get("/:id", UserController.details);
